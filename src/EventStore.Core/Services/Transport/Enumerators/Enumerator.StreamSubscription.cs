587708a6e20170b9094ef99c34553aa74a540896
JOSE FUXA <9057699+j-fuxa@users.noreply.github.com>
2025-08-12 13:14:15 -0400

    Changed log levels of gRPC enumerators (#24)
    Changed: Log levels of gRPC enumerators.
    
    Signed-off-by: JOSE FUXA <9057699+j-fuxa@users.noreply.github.com>

--- src/EventStore.Core/Services/Transport/Enumerators/Enumerator.StreamSubscription.cs
+++ src/EventStore.Core/Services/Transport/Enumerators/Enumerator.StreamSubscription.cs
@@ -76,7 +76,7 @@ namespace EventStore.Core.Services.Transport.Enumerators {
 					return ValueTask.CompletedTask;
 				}
 
-				Log.Debug("Subscription {subscriptionId} to {streamName} disposed.", _subscriptionId,
+				Log.Verbose("Subscription {subscriptionId} to {streamName} disposed.", _subscriptionId,
 					_streamName);
 				_disposed = true;
 
@@ -138,7 +138,7 @@ namespace EventStore.Core.Services.Transport.Enumerators {
 
 			private async Task MainLoop(StreamRevision? checkpointRevision, CancellationToken ct) {
 				try {
-					Log.Information("Subscription {subscriptionId} to {streamName} has started at checkpoint {streamRevision:N0}",
+					Log.Debug("Subscription {subscriptionId} to {streamName} has started at checkpoint {streamRevision:N0}",
 						_subscriptionId, _streamName, checkpointRevision?.ToString() ?? "Start");
 
 					var confirmationLastEventNumber = await SubscribeToLive();
@@ -164,7 +164,7 @@ namespace EventStore.Core.Services.Transport.Enumerators {
 						Log.Error(ex, "Subscription {subscriptionId} to {streamName} experienced an error.", _subscriptionId, _streamName);
 					_channel.Writer.TryComplete(ex);
 				} finally {
-					Log.Information("Subscription {subscriptionId} to {streamName} has ended.", _subscriptionId, _streamName);
+					Log.Debug("Subscription {subscriptionId} to {streamName} has ended.", _subscriptionId, _streamName);
 				}
 			}
 
