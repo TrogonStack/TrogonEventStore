126509170701d8e848848522d4edb39de71e6a90
Yordis Prieto <yordis.prieto@gmail.com>
2026-04-12 20:31:31 +0100

    fix(startup): avoid requiring the MD5 plugin by default (#43)
    - normal startup should not depend on an external MD5 plugin when the
    built-in provider works
    - plugin-backed MD5 should be reserved for environments where the
    built-in provider cannot be activated
    - reducing the default plugin dependency keeps this migration step small
    while leaving a clearer path for a later hashing redesign
    
    Signed-off-by: Yordis Prieto <yordis.prieto@gmail.com>

--- src/EventStore.Common/Exceptions/InvalidConfigurationException.cs
+++ src/EventStore.Common/Exceptions/InvalidConfigurationException.cs
@@ -5,4 +5,7 @@ namespace EventStore.Common.Exceptions;
 public class InvalidConfigurationException : Exception
 {
 	public InvalidConfigurationException(string message) : base(message) { }
+
+	public InvalidConfigurationException(string message, Exception innerException) : base(message,
+		innerException) { }
 }
