797407fee01770cc977c507a716d49fe8edaebfc
Yordis Prieto <yordis.prieto@gmail.com>
2026-04-22 13:59:23 -0400

    fix(tests): wait for persisted epochs instead of startup signals
    Signed-off-by: Yordis Prieto <yordis.prieto@gmail.com>

--- src/EventStore.Core.Tests/Integration/when_a_single_node_is_restarted_multiple_times.cs
+++ src/EventStore.Core.Tests/Integration/when_a_single_node_is_restarted_multiple_times.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -29,27 +30,46 @@ public class when_a_single_node_is_restarted_multiple_times<TLogFormat, TStreamI
 
 	protected override async Task Given()
 	{
-		_epochIds.Add(await GetLastEpochId());
+		Guid? previousEpochId = null;
+		var epochId = await GetLastEpochId(previousEpochId);
+		_epochIds.Add(epochId);
+		previousEpochId = epochId;
 
 		for (int i = 0; i < _numberOfNodeStarts - 1; i++)
 		{
 			await ShutdownNode();
 			await StartNode();
-			_epochIds.Add(await GetLastEpochId());
+			epochId = await GetLastEpochId(previousEpochId);
+			_epochIds.Add(epochId);
+			previousEpochId = epochId;
 		}
 
 		await base.Given();
 	}
 
-	private async Task<Guid> GetLastEpochId()
+	private async Task<Guid> GetLastEpochId(Guid? previousEpochId)
 	{
 		_logFormat ??= LogFormatHelper<TLogFormat, TStreamId>.LogFormatFactory.Create(new() {
 			IndexDirectory = GetFilePathFor("epoch-index"),
 		});
 
-		await _node.Started.WaitAsync(RestartTimeout);
 		await _node.WaitForTcpEndPoint().WaitAsync(RestartTimeout);
+		var wait = Stopwatch.StartNew();
+		while (wait.Elapsed < RestartTimeout)
+		{
+			var epochId = await TryGetLastEpochId();
+			if (epochId is { } currentEpochId && currentEpochId != previousEpochId)
+				return currentEpochId;
 
+			await Task.Delay(TimeSpan.FromMilliseconds(100));
+		}
+
+		throw new TimeoutException(
+			$"Expected startup to persist a new epoch before restart assertions. Previous epoch: {previousEpochId}");
+	}
+
+	private async Task<Guid?> TryGetLastEpochId()
+	{
 		var bus = new SynchronousScheduler(nameof(when_a_single_node_is_restarted_multiple_times<TLogFormat, TStreamId>));
 		await using var writer = new TFChunkWriter(_node.Db);
 		writer.Open();
@@ -71,9 +91,7 @@ public class when_a_single_node_is_restarted_multiple_times<TLogFormat, TStreamI
 			Guid.NewGuid());
 		await epochManager.Init(CancellationToken.None);
 
-		var epoch = epochManager.GetLastEpoch();
-		Assert.That(epoch, Is.Not.Null, "Expected startup to persist an epoch before restart assertions.");
-		return epoch.EpochId;
+		return epochManager.GetLastEpoch()?.EpochId;
 	}
 
 	[OneTimeTearDown]
