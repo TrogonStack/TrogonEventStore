9aa065a9f9e416576dc277c94474717022cae55a
Yordis Prieto <yordis.prieto@gmail.com>
2026-05-03 14:18:32 -0400

    fix(grpc): preserve user create coverage
    Signed-off-by: Yordis Prieto <yordis.prieto@gmail.com>

--- src/EventStore.Core.Tests/Services/Transport/Grpc/UsersTests/CreateTests.cs
+++ src/EventStore.Core.Tests/Services/Transport/Grpc/UsersTests/CreateTests.cs
@@ -34,6 +34,54 @@ public class CreateTests {
 	}
 
 	[TestFixture(typeof(LogFormat.V2), typeof(string))]
+	public class when_creating_user_with_unicode_name<TLogFormat, TStreamId> : GrpcSpecification<TLogFormat, TStreamId> {
+		private UsersClient _client;
+		private DetailsResp _details;
+
+		protected override async Task Given() {
+			_client = new UsersClient(Channel);
+			await Task.CompletedTask;
+		}
+
+		protected override async Task When() {
+			await _client.CreateAsync(CreateRequest("码ou£ro码", "Unicode Test User", "ou码码ro"),
+				GetCallOptions(AdminCredentials));
+			_details = await ReadSingleDetail(_client, "码ou£ro码", GetCallOptions(AdminCredentials));
+		}
+
+		[Test]
+		public void stores_the_user_details() {
+			Assert.AreEqual("码ou£ro码", _details.UserDetails.LoginName);
+			Assert.AreEqual("Unicode Test User", _details.UserDetails.FullName);
+			Assert.AreEqual(new[] {"admin", "other"}, _details.UserDetails.Groups.ToArray());
+		}
+	}
+
+	[TestFixture(typeof(LogFormat.V2), typeof(string))]
+	public class when_creating_existing_user_with_same_password<TLogFormat, TStreamId>
+		: GrpcSpecification<TLogFormat, TStreamId> {
+		private UsersClient _client;
+		private DetailsResp _details;
+
+		protected override async Task Given() {
+			_client = new UsersClient(Channel);
+			await _client.CreateAsync(CreateRequest("create-idempotent-test-user", "Create Test User", "Pa55w0rd!"),
+				GetCallOptions(AdminCredentials));
+		}
+
+		protected override async Task When() {
+			await _client.CreateAsync(CreateRequest("create-idempotent-test-user", "Create Test User", "Pa55w0rd!"),
+				GetCallOptions(AdminCredentials));
+			_details = await ReadSingleDetail(_client, "create-idempotent-test-user", GetCallOptions(AdminCredentials));
+		}
+
+		[Test]
+		public void leaves_the_existing_user_available() {
+			Assert.AreEqual("create-idempotent-test-user", _details.UserDetails.LoginName);
+		}
+	}
+
+	[TestFixture(typeof(LogFormat.V2), typeof(string))]
 	public class when_creating_existing_user_with_different_password<TLogFormat, TStreamId>
 		: GrpcSpecification<TLogFormat, TStreamId> {
 		private UsersClient _client;
@@ -59,6 +107,36 @@ public class CreateTests {
 		public void returns_failed_precondition() {
 			Assert.IsNotNull(_exception);
 			Assert.AreEqual(StatusCode.FailedPrecondition, _exception.Status.StatusCode);
+		}
+	}
+
+	[TestFixture(typeof(LogFormat.V2), typeof(string))]
+	public class when_creating_user_without_admin_credentials<TLogFormat, TStreamId>
+		: GrpcSpecification<TLogFormat, TStreamId> {
+		private const string ActorLogin = "create-denied-actor";
+		private const string Password = "Pa55w0rd!";
+		private UsersClient _client;
+		private RpcException _exception;
+
+		protected override async Task Given() {
+			_client = new UsersClient(Channel);
+			await _client.CreateAsync(CreateRequest(ActorLogin, "Create Denied Actor", Password),
+				GetCallOptions(AdminCredentials));
+		}
+
+		protected override async Task When() {
+			try {
+				await _client.CreateAsync(CreateRequest("create-denied-target", "Create Denied Target", Password),
+					GetCallOptions((ActorLogin, Password)));
+			} catch (RpcException ex) {
+				_exception = ex;
+			}
+		}
+
+		[Test]
+		public void returns_permission_denied() {
+			Assert.IsNotNull(_exception);
+			Assert.AreEqual(StatusCode.PermissionDenied, _exception.Status.StatusCode);
 		}
 	}
 
