c896c26e8b6403503b94267d01b7b1609ccecc76
Yordis Prieto <yordis.prieto@gmail.com>
2026-05-11 03:13:06 -0400

    chore(repo): trim config whitespace (#278)
    - Keep repository maintenance files aligned with the whitespace rules
    they define.
    
    Signed-off-by: Yordis Prieto <yordis.prieto@gmail.com>

--- dev-env/rm-cont.sh
+++ dev-env/rm-cont.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
-docker container stop $(docker container list -qa) 
-docker container rm -f $(docker container list -qa) 
+docker container stop $(docker container list -qa)
+docker container rm -f $(docker container list -qa)
