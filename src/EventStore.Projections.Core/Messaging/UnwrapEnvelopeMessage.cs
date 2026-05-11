461bf02eafd4f47b9ecc5cbb10a3cadeb745c0c1
Yordis Prieto <yordis.prieto@gmail.com>
2026-05-11 17:04:26 -0400

    fix(projections): identify unwrap envelope work
    Signed-off-by: Yordis Prieto <yordis.prieto@gmail.com>

--- src/EventStore.Projections.Core/Messaging/UnwrapEnvelopeMessage.cs
+++ src/EventStore.Projections.Core/Messaging/UnwrapEnvelopeMessage.cs
@@ -8,14 +8,21 @@ namespace EventStore.Projections.Core.Messaging;
 public partial class UnwrapEnvelopeMessage : Message
 {
 	private readonly Action _action;
+	private readonly string _extraInformation;
 
-	public UnwrapEnvelopeMessage(Action action)
+	public UnwrapEnvelopeMessage(Action action, string extraInformation)
 	{
 		_action = action;
+		_extraInformation = extraInformation;
 	}
 
 	public Action Action
 	{
 		get { return _action; }
+	}
+
+	public override string ToString()
+	{
+		return $"{base.ToString()}, Extra Information: {_extraInformation}";
 	}
 }
