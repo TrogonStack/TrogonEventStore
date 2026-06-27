8ff202ecbc9b29476ed7d07c723434cfdf124fed
Yordis Prieto <yordis.prieto@gmail.com>
2026-06-27 18:39:13 -0400

    feat(common): reduce event batch allocation pressure (#392)
    - Reduce allocation pressure in internal stream-processing paths that
    often accumulate zero or one item before publishing a batch.
    
    Signed-off-by: Yordis Prieto <yordis.prieto@gmail.com>

--- src/EventStore.Common/Utils/LowAllocReadOnlyMemory.cs
+++ src/EventStore.Common/Utils/LowAllocReadOnlyMemory.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -46,12 +47,83 @@ public readonly struct LowAllocReadOnlyMemory<T>
 	public ReadOnlySpan<T>.Enumerator GetEnumerator() => Span.GetEnumerator();
 
 	public T[] ToArray() => Span.ToArray();
+
+	public readonly struct Builder
+	{
+		private readonly ReadOnlyMemory<T> _many;
+		private readonly T _single;
+		private readonly bool _hasSingle;
+
+		public Builder(T item)
+		{
+			_single = item;
+			_hasSingle = true;
+		}
+
+		public Builder(ReadOnlyMemory<T> items)
+		{
+			if (items.Span is [var item])
+			{
+				_single = item;
+				_hasSingle = true;
+				return;
+			}
+
+			_many = items;
+		}
+
+		public static Builder Empty => default;
+
+		public int Count => _hasSingle ? 1 : _many.Length;
+
+		public Builder Add(T item)
+		{
+			if (_hasSingle)
+			{
+				return new Builder(new[] { _single, item });
+			}
+
+			if (_many.IsEmpty)
+			{
+				return new Builder(item);
+			}
+
+			var items = new T[_many.Length + 1];
+			_many.CopyTo(items);
+			items[^1] = item;
+			return new Builder(items);
+		}
+
+		public LowAllocReadOnlyMemory<T> Build() => _hasSingle
+			? new LowAllocReadOnlyMemory<T>(_single)
+			: new LowAllocReadOnlyMemory<T>(_many);
+	}
 }
 
 public static class LowAllocReadOnlyMemory
 {
 	public static LowAllocReadOnlyMemory<T> Create<T>(ReadOnlySpan<T> items) =>
-		items is [var item]
-			? new LowAllocReadOnlyMemory<T>(item)
-			: new LowAllocReadOnlyMemory<T>(items.ToArray());
+		items switch
+		{
+			[] => LowAllocReadOnlyMemory<T>.Empty,
+			[var item] => new LowAllocReadOnlyMemory<T>(item),
+			_ => new LowAllocReadOnlyMemory<T>(items.ToArray())
+		};
+
+	public static LowAllocReadOnlyMemory<T> ToLowAllocReadOnlyMemory<T>(this IList<T> items)
+	{
+		if (items is null || items.Count == 0)
+		{
+			return LowAllocReadOnlyMemory<T>.Empty;
+		}
+
+		if (items.Count == 1)
+		{
+			return new LowAllocReadOnlyMemory<T>(items[0]);
+		}
+
+		var array = new T[items.Count];
+		items.CopyTo(array, 0);
+		return new LowAllocReadOnlyMemory<T>(array);
+	}
 }
