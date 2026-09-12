d3f495ff9da7a5e80aaaa875a2bb669e09451291
Yordis Prieto <yordis.prieto@gmail.com>
2026-09-12 18:21:41 -0400

    feat(test-client): keep operational scenarios usable without TCP
    Signed-off-by: Yordis Prieto <yordis.prieto@gmail.com>

--- src/EventStore.TestClient/Program.cs
+++ src/EventStore.TestClient/Program.cs
@@ -29,22 +29,17 @@ internal static class Program
 	/// <param name="whatIf">Print effective configuration to console and then exit.</param>
 	/// <param name="command"></param>
 	/// <param name="host">The host to bind to.</param>
-	/// <param name="tcpPort">The port to run the TCP server on.</param>
 	/// <param name="httpPort">The port to run the HTTP server on.</param>
 	/// <param name="timeout"></param>
-	/// <param name="readWindow"></param>
-	/// <param name="writeWindow"></param>
-	/// <param name="pingWindow"></param>
-	/// <param name="reconnect"></param>
 	/// <param name="useTls"></param>
 	/// <param name="tlsValidateServer"></param>
-	/// <param name="connectionString">The connection string to use when connecting to the server. Not used by the raw TCP client</param>
+	/// <param name="connectionString">The connection string to use when connecting to the server.</param>
 	/// <param name="statsFormat">The format for the stats log.</param>
 	/// <returns></returns>
 	public static async Task<int> Main(bool version = false, FileInfo? log = null, bool whatIf = false,
-		string[]? command = null, string host = "localhost", int tcpPort = 1113, int httpPort = 2113,
-		int timeout = Timeout.Infinite, int readWindow = 2000, int writeWindow = 2000, int pingWindow = 2000,
-		bool reconnect = true, bool useTls = false, bool tlsValidateServer = false, string connectionString = "",
+		string[]? command = null, string host = "localhost", int httpPort = 2113,
+		int timeout = Timeout.Infinite,
+		bool useTls = false, bool tlsValidateServer = false, string connectionString = "",
 		StatsFormat statsFormat = StatsFormat.Csv)
 	{
 		Log.Logger = EventStoreLoggerConfiguration.ConsoleLog;
@@ -63,11 +58,6 @@ internal static class Program
 				Timeout = timeout,
 				HttpPort = httpPort,
 				Host = host,
-				TcpPort = tcpPort,
-				Reconnect = reconnect,
-				PingWindow = pingWindow,
-				ReadWindow = readWindow,
-				WriteWindow = writeWindow,
 				UseTls = useTls,
 				TlsValidateServer = tlsValidateServer,
 				ConnectionString = connectionString,
