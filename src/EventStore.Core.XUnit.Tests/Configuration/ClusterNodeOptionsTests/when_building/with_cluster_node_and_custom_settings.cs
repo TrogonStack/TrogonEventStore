2e33e61a305af73e92aa7e82550cf7c688c5678d
Yordis Prieto <yordis.prieto@gmail.com>
2026-09-21 13:38:01 -0400

    fix(tests): follow the gRPC replication endpoint
    Signed-off-by: Yordis Prieto <yordis.prieto@gmail.com>

--- src/EventStore.Core.XUnit.Tests/Configuration/ClusterNodeOptionsTests/when_building/with_cluster_node_and_custom_settings.cs
+++ src/EventStore.Core.XUnit.Tests/Configuration/ClusterNodeOptionsTests/when_building/with_cluster_node_and_custom_settings.cs
@@ -155,7 +155,6 @@ public class with_custom_replication_port_advertise_as<TLogFormat, TStreamId> : 
 		{
 			Assert.AreEqual(_options.Interface.ReplicationPort, _node.NodeInfo.ReplicationEndPoint.GetPort());
 			Assert.AreEqual(3112, _node.GossipAdvertiseInfo.ReplicationEndPoint.Port);
-			Assert.AreEqual(3112, _node.GossipAdvertiseInfo.InternalSecureTcp.Port);
 		});
 	}
 }
